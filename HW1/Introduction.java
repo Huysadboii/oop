571316b5b5be4fabfe36052c2a57c5336742fca7
Huy <24020161@vnu.edu.vn>
2025-09-24 11:02:32 +0700

    HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,20 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyễn Đức Huy'\t'24020161'\t'2526I_INT2204_1'\t'Huysadboii'\t'24020161@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                "\n" +
+                "Take one down, pass it around, \n" +
+                "\n" +
+                "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                "\n" +
+                "Take one down, pass it around,\n" +
+                "\n" +
+                "...\n" +
+                "\n" +
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "\n" +
+                "Take one down, pass it around,\n" +
+                "\n" +
+                "No more bottles of beer on the wall.");
+    }
+}
